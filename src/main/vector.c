36b8817b445dac3ebe0e845689e0bf39866331aa
home <rubenstfj@LAPTOP-OIKFAVCD>
2024-01-22 23:39:48 +0000

    fixed mouse movement

--- src/main/vector.c
+++ src/main/vector.c
@@ -43,24 +43,24 @@ void	update_vector(t_object *object)
 
 	mouse[X] = 0;
 	mouse[Y] = 0;
-	// mlx_mouse_get_pos(engine()->game.mlx, engine()->game.win,
-	// 	&mouse[X], &mouse[Y]);
-	// distance = mouse[X] - (engine()->map.pixelwidth / 2);
-	// engine()->map.middle -= mouse[Y] - (engine()->map.pixelheight / 2); 
-	// if (engine()->map.middle < 0)
-	// 	engine()->map.middle = 0;
-	// if (engine()->map.middle >= engine()->map.pixelheight)
-	// 	engine()->map.middle = engine()->map.pixelheight;
-	// rotate_vector(object->vector, distance / 8);
-	// rotate_vector(object->sidevector, distance / 8);
-	distance = *on('e') - *on('q');
-	if (distance)
-	{
-		rotate_vector(object->vector, distance);
-		rotate_vector(object->sidevector, distance);
-	}
+	mlx_mouse_get_pos(engine()->game.mlx, engine()->game.win,
+		&mouse[X], &mouse[Y]);
+	distance = mouse[X] - (engine()->map.pixelwidth / 2);
+	engine()->map.middle -= mouse[Y] - (engine()->map.pixelheight / 2); 
+	if (engine()->map.middle < 0)
+		engine()->map.middle = 0;
+	if (engine()->map.middle >= engine()->map.pixelheight)
+		engine()->map.middle = engine()->map.pixelheight;
+	rotate_vector(object->vector, distance / 8);
+	rotate_vector(object->sidevector, distance / 8);
+	// distance = *on('e') - *on('q');
+	// if (distance)
+	// {
+	// 	rotate_vector(object->vector, distance);
+	// 	rotate_vector(object->sidevector, distance);
+	// }
 	object->cross = perpendicular_line(object->vector,
 			(double []){object->x, object->y});
-	// mlx_mouse_move(engine()->game.mlx, engine()->game.win,
-	// 	(engine()->map.pixelwidth / 2), (engine()->map.pixelheight / 2));
+	mlx_mouse_move(engine()->game.mlx, engine()->game.win,
+		(engine()->map.pixelwidth / 2), (engine()->map.pixelheight / 2));
 }
